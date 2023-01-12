949fdd3ac184a26d1d4ff29eecd80b3d6320bc6d
puranchandralohar <clpuran95@gmail.com>
2023-01-12 16:17:21 +0530

    add

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+let captchaButton = document.getElementById('captcha_reload');
+let captchaText = document.getElementById('captcha');
+
+
 let captchaLetters = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let captchaButton = document.getElementById('captcha_reload');
-let captchaText = document.getElementById('captcha')
+
 
 captchaButton.addEventListener('click', (e)=>{
     e.preventDefault();
-
     let result = generateCaptcha()
     captchaText.innerText = result
 
